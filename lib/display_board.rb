931988a6fc91d970342956791f19090cc9006350
wei chen <weichen9083@gmail.com>
2019-08-20 16:16:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,20 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "   |   |   "
+  board[0] = a
+  board[1] = b
+  board[2] = c
+  board[3] = d
+  board[4] = e
+  board[5] = f
+  board[6] = g 
+  board[7] = h 
+  board[8] = i
+  puts " #{a} | #{b} | #{c} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{d} | #{e} | #{f} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{g} | #{h} | #{i} "
 end 
 
-display_board(1)
+
